0a14e90e349146c6187de81bffdafcba74c19de1
Kornil777 <kornilovsanya@yandex.ru>
2025-07-19 01:22:47 +0500

    ДЗ по JS

---
+++ Homework1/script.js
@@ -0,0 +1,73 @@
+// задача№1
+let a =10;
+alert(a);
+a =20;
+alert(20);
+
+// задача№2
+let iphone  =2007;
+alert(iphone);
+
+// задача№3
+let name = "Брендан Эйх";
+alert(name);
+
+// задача№4
+let a =10;
+let b =2;
+let sum = a+b;
+alert(sum);
+let diffference = a-b;
+alert(diffference);
+let product = a*b;
+alert(product);
+let quotient = a/b;
+alert(quotient);
+
+// задача №5
+let a =2;
+let result = 2**5;
+alert(result);
+
+// задача №6
+let a =9;
+let b =2;
+let result = a%b;
+alert(result);
+
+// задача№7
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num++;
+num--;
+alert(num);
+
+// задача №8
+let age = prompt("Сколько вам лет?")
+alert(age);
+
+// задача№9
+const user = {
+    name: String('User'),
+    age: Number(20),
+    isAdmin: Boolean(true)
+}
+задача №10
+let name = prompt('Назовите Ваше Имя');
+alert(`Привет, ${name}`);
+
+// доп.задание
+let number = Number(prompt('Загадайте число'));
+let a = number*2;
+alert(a);
+let b =a+10;
+alert(b);
+let c =b/2;
+alert(c);
+let total =c-10;
+alert("Ответ равен 5");
+
+
